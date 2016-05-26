e0a5519b66436eb6ac33e6865497392c4df50e49
Gallen <gallen@GallendeMacBook-Pro.local>
2016-05-26 15:33:13 +0800

    Add props: isCover

--- LoadingEffect.js
+++ LoadingEffect.js
@@ -5,10 +5,31 @@ import {
   Text,
   Platform,
   ProgressBarAndroid,
+  Dimensions,
 } from 'react-native';
 
 const LoadingEffect = (props) => {
-  const customStyles = {
+  const styles = {
+    container: {
+      position: 'absolute',
+      backgroundColor: 'transparent',
+      justifyContent: 'center',
+      alignItems: 'center',
+      top: (Dimensions.get('window').height - props.overlayHeight) / 2,
+      left: (Dimensions.get('window').width - props.overlayWidth) / 2,
+    },
+
+    container_cover: {
+      position: 'absolute',
+      backgroundColor: 'transparent',
+      justifyContent: 'center',
+      alignItems: 'center',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+    },
+
     overlay: {
       alignItems: 'center',
       justifyContent: 'center',
@@ -30,12 +51,12 @@ const LoadingEffect = (props) => {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={customStyles.overlay}>
+    <View style={props.isCover ? styles.container_cover : styles.container}>
+      <View style={styles.overlay}>
         {(Platform.OS === 'ios')
           ? <ActivityIndicatorIOS color={props.color} size={props.size} />
           : <ProgressBarAndroid styleAttr={props.size} color={props.color} />}
-        <Text style={customStyles.text}>{props.text}</Text>
+        <Text style={styles.text}>{props.text}</Text>
       </View>
     </View>
   );
@@ -51,6 +72,7 @@ LoadingEffect.propTypes = {
   text: React.PropTypes.string,
   textColor: React.PropTypes.string,
   textFontSize: React.PropTypes.number,
+  isCover: React.PropTypes.bool,
 };
 
 LoadingEffect.defaultProps = {
@@ -63,19 +85,7 @@ LoadingEffect.defaultProps = {
   text: 'Loading',
   textColor: '#eeeeee',
   textFontSize: 14,
-};
-
-const styles = {
-  container: {
-    position: 'absolute',
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-    alignItems: 'center',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
+  isCover: true,
 };
 
 export default LoadingEffect;
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Build base on  [react-native-loading-effect](https://github.com/Iliker/react-nat
 - Use Stateless Functions
 - Rmove `showLoadingEffect` Function
 - Rmove `dismissLoadingEffect` Function
+- Add props `isCover`
 
 ## Usage
 ```js
@@ -26,3 +27,16 @@ export class YourComponent extends React.Component {
   }
 }
 ```
+
+## Props
+
+- **`isVisible`** _(Bool)_ - isRequired.
+- **`size`** _(String)_ - size of the indicator. Small has a height of 20, large has a height of 36.
+- **`color`** _(Number)_ - color of the indicator. Default gray.
+- **`overlayWidth`** _(Number)_ - overlay width.
+- **`overlayHeight`** _(Number)_ - overlay height.
+- **`overlayColor`** _(String)_ - overlay color.
+- **`text`** _(String)_ - text. Default Loading.
+- **`textColor`** _(String)_ - text color.
+- **`textFontSize`** _(Number)_ - text font size.
+- **`isCover`** _(Bool)_ - cover the full page. You can click other Components when false. Default true.
